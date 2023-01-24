c46ed4fabc3777dadf44c72558761780ff99f9c6
sximenez <70152164+sximenez@users.noreply.github.com>
2023-01-24 21:39:32 +0100

    update: read function

--- employeeDirectory/model/read.js
+++ employeeDirectory/model/read.js
@@ -111,48 +111,52 @@ export function readCard() {
     <div class="overflow-y-auto overflow-x-hidden h-full w-full">
       <div class="flex flex-col w-full h-full md:grow md:h-auto gap-x-6">
 
-        <a href="#/" class="w-fit mb-4 text-sm font-medium text-gray-900 hover:underline">
-          ← Back  
-        </a>
-
         <!-- Image block -->
-        <div class="flex flex-col md:w-[300px] md:fixed md:h-[calc(100vh - 70px)] md:overflow-y-auto border rounded-lg p-5 h-fit"> 
+        <div class="flex flex-col md:w-[300px] md:fixed md:h-[calc(100vh - 70px)] md:overflow-y-auto h-fit"> 
         
-          <div class="w-full flex justify-center my-3 h-64">
-            <img class="hidden h-fit w-fit rounded-full" src="${data.photo}"
-              alt="Profile picture of ${data.name}">
-              <div class="w-64 h-64 bg-cover bg-center border rounded-full"
-                style="background-image: url('${data.photo}')"></div>
-          </div>
+          <a href="#/" class="w-fit mb-4 text-sm font-medium text-gray-900 hover:underline">
+            ← Back  
+          </a>
+        
+          <div class="border rounded-lg p-5">
+          
+            <div class="w-full flex justify-center my-3 h-64">
+              <img class="hidden h-fit w-fit rounded-full" src="${data.photo}"
+                alt="Profile picture of ${data.name}">
+                <div class="w-64 h-64 bg-cover bg-center border rounded-full"
+                  style="background-image: url('${data.photo}')"></div>
+            </div>
 
-          <!-- Buttons -->
-          <div class="p-6">
+            <!-- Buttons -->
+            <div class="p-6">
 
-            <div class="flex items-center justify-evenly space-x-3 sm:space-x-4">
+              <div class="flex items-center justify-evenly space-x-3 sm:space-x-4">
 
-              <a href="#/update"
-                class="text-blue-700 inline-flex items-center border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                <svg aria-hidden="true" class="mr-1 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg">
-                  <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z">
-                  </path>
-                  <path fill-rule="evenodd"
-                    d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
-                    clip-rule="evenodd"></path>
-                </svg>
-                Edit
-              </a>
+                <a href="#/update"
+                  class="text-blue-700 inline-flex items-center border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                  <svg aria-hidden="true" class="mr-1 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z">
+                    </path>
+                    <path fill-rule="evenodd"
+                      d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+                      clip-rule="evenodd"></path>
+                  </svg>
+                  Edit
+                </a>
 
-              <a id="delete-button" href="#/delete"
-                class="inline-flex items-center text-red-600 border border-red-600 hover:bg-red-600 hover:text-white focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900">
-                <svg aria-hidden="true" class="w-5 h-5 mr-1.5 -ml-1" fill="currentColor" viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd"
-                    d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                    clip-rule="evenodd"></path>
-                </svg>
-                Delete
-              </a>
+                <a id="delete-button" href="#/delete"
+                  class="inline-flex items-center text-red-600 border border-red-600 hover:bg-red-600 hover:text-white focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900">
+                  <svg aria-hidden="true" class="w-5 h-5 mr-1.5 -ml-1" fill="currentColor" viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd"
+                      d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                      clip-rule="evenodd"></path>
+                  </svg>
+                  Delete
+                </a>
+
+              </div>
 
             </div>
 
